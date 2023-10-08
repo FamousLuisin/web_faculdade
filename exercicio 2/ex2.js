36a85964004caf82dd5bf7fcc8c3ad798633e5ba
FamousLuisin <lufimelo@outlook.com>
2023-10-08 16:35:47 -0300

    atividades faculdade

---
+++ exercicio 2/ex2.js
@@ -0,0 +1,17 @@
+function Matriz(matriz) {
+    let transMatriz = []
+    for (let len = 0; len < matriz[0].length; len++) {
+        let nlinha = []
+        for (const linha of matriz) nlinha.push(linha[len]) 
+        transMatriz.push(nlinha)
+    }
+    return transMatriz
+}
+
+let matriz = [
+    [1, 2, 3, 4],
+    [4, 5, 6, 6],
+    [7, 8, 9, 91]
+]
+
+console.log(Matriz(matriz))
